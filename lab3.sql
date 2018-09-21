5dda3f8b6691083e435f31cfaea4ec3e526fe617
TolkynKurmnayeva <40239658+TolkynKurmnayeva@users.noreply.github.com>
2018-09-21 14:04:39 +0600

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,3 +1,4 @@
+create lab3;
 CREATE TABLE departments (
   code INTEGER PRIMARY KEY,
   name VARCHAR(255) NOT NULL ,
@@ -17,7 +18,7 @@ CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL ,
   lastname VARCHAR(255) NOT NULL ,
-  city VARCHAR(255),
+  city VARCHAR(255)
 
 );
 
@@ -52,3 +53,46 @@ INSERT INTO customers(name,lastname, city) VALUES('Shawn','Efron', 'Astana');
 INSERT INTO customers(name,lastname, city) VALUES('Eric','Gomez', 'Shymkent');
 INSERT INTO customers(name,lastname, city) VALUES('Elizabeth','Tailor', 'Almaty');
 INSERT INTO customers(name,lastname, city) VALUES('Julia','Adams', 'Astana');
+	
+
+SELECT * from departments;
+SELECT * from employees;
+SELECT * from customers;
+
+select * from employees
+SELECT lastname from employees;
+SELECT DISTINCT lastname from employees;
+SELECT lastname from employees WHERE lastname = 'Smith';
+SELECT lastname from employees WHERE lastname = 'Smith' or lastname = 'Doe';
+SELECT *  from employees where department = '14';
+SELECT *  from employees where department = '37' or department = '77' ;
+SELECT sum(budget) from departments;
+SELECT department, count(*) FROM employees GROUP BY department;
+SELECT department, count(*) FROM employees GROUP BY department HAVING count (*)>2;
+
+SELECT department, count(*) FROM employees GROUP BY department;
+SELECT code, budget FROM departments ORDER BY budget DESC LIMIT 2 OFFSET 2;
+SELECT name, lastname FROM employees WHERE department = (SELECT code FROM departments ORDER BY budget ASC LIMIT 1);
+SELECT name, lastname FROM employees WHERE city = 'Almaty'
+SELECT * from departments  where  budget>60000 order by budget asc, code desc;
+UPDATE departments SET budget = budget * 0.9 WHERE budget = (SELECT budget FROM departments ORDER BY budget ASC LIMIT 1);
+UPDATE EMPLOYEES
+SET department = (SELECT code FROM departments WHERE departments.name = 'IT')
+
+WHERE department = (SELECT code FROM departments WHERE departments.name = 'Research');
+DELETE FROM employees WHERE department = (SELECT code FROM departments WHERE departments.name = 'IT');
+
+
+DELETE FROM employees;
+
+SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM customers;
+
+
+
+
+
+
